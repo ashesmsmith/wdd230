d542ff535853012e4efd1d000133f6841e6c560b
ashesmsmith <ashesmsmith@gmail.com>
2024-01-17 20:00:32 -0700

    attempt to correct js error

--- lesson02/scripts/getdates.js
+++
@@ -1,9 +0,0 @@
-const copyrightYear = document.querySelector('#copyrightYear');
-const options = {
-    year: 'numeric'
-}
-
-copyrightYear.innerHTML = new Date().toLocaleDateString('en-US', options);
-
-const lastModified = document.lastModified;
-document.getElementById('lastModified').innerHTML = lastModified;
