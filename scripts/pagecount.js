e94b675f6006cc1b7dd97065eb0be380e33bb19e
ashesmsmith <ashesmsmith@gmail.com>
2024-02-17 11:32:53 -0700

    page visits and optimized images

---
+++ scripts/pagecount.js
@@ -0,0 +1,20 @@
+const pageVists = document.querySelector('#visits');
+
+//GET the value for numVisits-ls key in localStorage 
+//If no numVisits key, assign 0 to the numVisits variable
+let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
+
+//determine number of visits and display
+if (numVisits !== 0) {
+    pageVists.textContent = numVisits;
+}
+
+else {
+    pageVists.textContent = 'Welcome!';
+}
+
+//increment by 1
+numVisits++;
+
+//new visits total saved in localStorage
+localStorage.setItem('numVisits-ls', numVisits);
