bdebbea1a5deae3fb94a7f1b5af8be98524fcb1e
ashesmsmith <ashesmsmith@gmail.com>
2024-02-19 13:41:05 -0700

    discover sidebar/images/message

--- chamber/scripts/getdate.js
+++ chamber/scripts/getdate.js
@@ -1,10 +1,21 @@
-// Last Modified
+// Last Modified - Footer
 const lastModified = document.lastModified;
 document.getElementById('lastModified').innerHTML = lastModified;
 
-// Copyright Year
+// Copyright Year - Footer
 const copyrightYear = document.querySelector('#copyrightYear');
 const options = {
     year: 'numeric'
 }
 copyrightYear.innerHTML = new Date().toLocaleDateString('en-US', options);
+
+// Current Date - Discover Page
+const currentDate = document.querySelector('.current-date');
+const options2 = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'   
+}
+
+currentDate.innerHTML = new Date().toLocaleDateString('en-US', options2);
---
+++ chamber/scripts/visitmessage.js
@@ -0,0 +1,49 @@
+const msToDays = 86400000; // milliseconds to days constant = 1000 ms/s * 60 s/m * 60 m/h * 24 h/day
+const messageDisplay = document.querySelector('#message');
+
+//Today
+const msDateToday = Date.now(); //timestamp
+let dateToday = new Date(msDateToday); //timestamp as date
+let formatDateToday = `${dateToday.getDate()}/${dateToday.getMonth() + 1}/${dateToday.getFullYear()}`;
+console.log(`msDateToday: ${msDateToday}`);
+
+//Last Visit
+let msLastVisit = getDate() || 0; //return last timestamp or 0
+let lastVisit = new Date(msLastVisit); //timestamp as date
+let formatLastVisit = `${lastVisit.getDate()}/${lastVisit.getMonth() + 1}/${lastVisit.getFullYear()}`;
+console.log(`msLastVisit: ${msLastVisit}`);
+
+console.log((msDateToday - 1708398094) / msToDays);
+
+//Processing
+if (msLastVisit === 0) {
+    messageDisplay.textContent = 'Welcome! Let us know if you have any questions.';
+    setDate();
+}
+
+else if (formatLastVisit === formatDateToday) {
+    messageDisplay.textContent = 'Back so soon! Awesome!';
+    setDate();
+}
+
+else {
+    let daysDifference = (msLastVisit - msDateToday) / msToDays;
+
+    if (daysDifference === 1) {
+        messageDisplay.textContent = `You last visited 1 day ago.`;
+        setDate();
+    }
+
+    else {
+        messageDisplay.textContent = `You last visited ${daysDifference.toFixed(0)} days ago.`;
+        setDate();
+    }
+}
+
+function setDate() {
+    localStorage.setItem('currentDate-ls', JSON.stringify(msDateToday));
+}
+
+function getDate() {
+    return JSON.parse(localStorage.getItem('currentDate-ls'));
+}
