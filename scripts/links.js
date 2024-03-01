0ab6f5c8d97c045007878c598b1c402d0ae66c8f
ashesmsmith <ashesmsmith@gmail.com>
2024-03-01 16:04:07 -0700

    home page json complete

--- scripts/links.js
+++ scripts/links.js
@@ -1,8 +1,31 @@
 const baseURL = 'https://ashesmsmith.github.io/wdd230/';
-var linksURL = 'https://ashesmsmith.github.io/wdd230/data/links.json';
+const linksURL = 'https://ashesmsmith.github.io/wdd230/data/links.json';
+const list = document.querySelector('#weeks');
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data.lessons); //Testing
+    // console.log(data); //Testing
+
+    displayLinks(data.lessons); // lessons array from json file
 }
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => { //look at each individual week/lesson from array
+        var weekNum = document.createElement('li');
+        weekNum.innerHTML = `${week.lesson}: `; 
+        
+        week.links.forEach((link) => { //look at each link from array of links on each week
+            var linkURL = document.createElement('a');
+            linkURL.setAttribute('href', link.url);
+            linkURL.setAttribute('target', '_blank');
+            linkURL.innerHTML = `${link.title}`;
+
+            weekNum.appendChild(linkURL);
+        })
+        
+        list.appendChild(weekNum);
+    })
+}
+
+getLinks();
