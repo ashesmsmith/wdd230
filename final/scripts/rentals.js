2aa1699523a2cd0297153b4797bacc1352b2f906
ashesmsmith <ashesmsmith@gmail.com>
2024-03-21 13:41:45 -0600

    rental page start

--- final/scripts/rentals.js
+++ final/scripts/rentals.js
@@ -1,13 +1,41 @@
 const url = 'https://ashesmsmith.github.io/wdd230/final/data/rentals.json';
-const table = document.querySelector('#pricing-table');
+const tableData = document.querySelector('tbody');
 
 async function getRentals () {
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(data);
+    displayRentals(data);
 }
 
+function displayRentals(data) {
+    data.rentals.forEach((rental) => {
 
+        let newRow = document.createElement('tr');
+
+        let type = document.createElement('td');
+        type.setAttribute('class', 'left-align');
+        type.innerHTML = rental.type;
+        let capacity = document.createElement('td');
+        capacity.innerHTML = rental.capacity;
+        let resHalf = document.createElement('td');
+        resHalf.innerHTML = `$${rental.reservehalf}`;
+        let resFull = document.createElement('td');
+        resFull.innerHTML = `$${rental.reservefull}`;
+        let walkInHalf = document.createElement('td');
+        walkInHalf.innerHTML = `$${rental.walkinhalf}`;
+        let walkInFull = document.createElement('td');
+        walkInFull.innerHTML = `$${rental.walkinfull}`;
+
+        newRow.appendChild(type);
+        newRow.appendChild(capacity);
+        newRow.appendChild(resHalf);
+        newRow.appendChild(resFull);
+        newRow.appendChild(walkInHalf);
+        newRow.appendChild(walkInFull);
+
+        tableData.appendChild(newRow);
+    })
+}
 
 getRentals();
